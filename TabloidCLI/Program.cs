2542a416d5c910b3ed132788b65dbc26851ecfb3
srcrank <salmacrank@gmail.com>
2021-06-16 10:09:23 -0500

    some additions to color

--- TabloidCLI/Program.cs
+++ TabloidCLI/Program.cs
@@ -7,6 +7,19 @@ namespace TabloidCLI
     {
         static void Main(string[] args)
         {
+            
+            //Console.WriteLine("=====================================");
+            //Console.ForegroundColor = ConsoleColor.White;
+            //// Let's go through all Console colors and set them as background
+            //Console.WriteLine("Choose a background color:");
+            //BGColor = Console.ReadLine();
+            //Console.BackgroundColor($"{BGColor}");
+            //foreach (ConsoleColor color in Enum.GetValues(typeof(ConsoleColor)))
+            //{
+            //    Console.BackgroundColor = color;
+            //    Console.WriteLine($"Background color set to {color}");
+            //}
+         
             Console.WriteLine("You have such a nice aura today!");
             Console.WriteLine(" ");
             // MainMenuManager implements the IUserInterfaceManager interface
