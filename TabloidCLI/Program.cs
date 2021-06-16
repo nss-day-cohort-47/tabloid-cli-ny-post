ef18d7b4cd1aa2984a671ce4b62e2b3342d82a28
Jackson <jacksonrgoodman@gmail.com>
2021-06-16 15:44:40 -0500

    jrg

--- TabloidCLI/Program.cs
+++ TabloidCLI/Program.cs
@@ -7,6 +7,22 @@ namespace TabloidCLI
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter a background color:");
+            Console.Write("> ");
+            var bg = Console.ReadLine();
+            Console.WriteLine("Please enter a text color: ");
+            Console.Write("> ");
+            var text = Console.ReadLine();
+            // Attempt to parse the colors that the user entered into their respective enum values.
+            // The new values of background and foreground will be set to the user input
+            if (Enum.TryParse(bg, out ConsoleColor background))
+            {
+                Console.BackgroundColor = background;
+            }
+            if (Enum.TryParse(text, out ConsoleColor foreground))
+            {
+                Console.ForegroundColor = foreground;
+            }
             Console.WriteLine("You have such a nice aura today!");
             Console.WriteLine(" ");
             // MainMenuManager implements the IUserInterfaceManager interface
