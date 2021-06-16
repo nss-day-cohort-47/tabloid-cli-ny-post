185a8efe886f0115127a38268fc491d049dd3d7f
Jackson <jacksonrgoodman@gmail.com>
2021-06-16 16:14:54 -0500

    color picker working (capital letters only :( )

--- TabloidCLI/Program.cs
+++ TabloidCLI/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Globalization;
 using TabloidCLI.UserInterfaceManagers;
+using System.Threading;
 
 namespace TabloidCLI
 {
@@ -7,24 +9,35 @@ namespace TabloidCLI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter a background color:");
+            Console.WriteLine("Please enter desired background color for this screen. ");
+            Console.WriteLine();
+            ConsoleColor[] consoleColors
+            = (ConsoleColor[])ConsoleColor
+                  .GetValues(typeof(ConsoleColor));
+            Console.WriteLine("List of available " + "Console Colors:");
+            foreach (var color in consoleColors)
+            Console.WriteLine(color);
+            Console.WriteLine();
+            Console.WriteLine("Please capitalize the first letter. ");
+            Console.WriteLine();
             Console.Write("> ");
-            var bg = Console.ReadLine();
-            Console.WriteLine("Please enter a text color: ");
-            Console.Write("> ");
-            var text = Console.ReadLine();
+            var screen = Console.ReadLine();
+            //var finalScreen = screen.ToString().ToUpper();
+            
+            //var finalText = text.ToString().ToUpper();
             // Attempt to parse the colors that the user entered into their respective enum values.
             // The new values of background and foreground will be set to the user input
-            if (Enum.TryParse(bg, out ConsoleColor background))
+            if (Enum.TryParse(screen, out ConsoleColor background))
             {
                 Console.BackgroundColor = background;
             }
-            if (Enum.TryParse(text, out ConsoleColor foreground))
-            {
-                Console.ForegroundColor = foreground;
-            }
+            
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            Thread.Sleep(300);
             Console.WriteLine("You have such a nice aura today!");
             Console.WriteLine(" ");
+            
             // MainMenuManager implements the IUserInterfaceManager interface
             IUserInterfaceManager ui = new MainMenuManager();
             while (ui != null)
