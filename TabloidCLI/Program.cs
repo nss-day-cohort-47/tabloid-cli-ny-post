af49d0d25b662f7b7d9d661007342d9d55f153b6
Jackson <jacksonrgoodman@gmail.com>
2021-06-17 11:19:54 -0500

    added int input, adaptive foreground color

--- TabloidCLI/Program.cs
+++ TabloidCLI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Globalization;
 using TabloidCLI.UserInterfaceManagers;
 using System.Threading;
@@ -9,34 +10,42 @@ namespace TabloidCLI
     {
         static void Main(string[] args)
         {
+
+
             Console.WriteLine("Please enter desired background color for this screen. ");
             Console.WriteLine();
-            ConsoleColor[] consoleColors
-            = (ConsoleColor[])ConsoleColor
-                  .GetValues(typeof(ConsoleColor));
-            Console.WriteLine("List of available " + "Console Colors:");
-            foreach (var color in consoleColors)
-            Console.WriteLine(color);
+            ConsoleColor[] colors = (ConsoleColor[])ConsoleColor.GetValues(typeof(ConsoleColor));
+            for (int i = 0; i < 16; i++)
+            {    
+                Console.WriteLine($"{i + 1}) {colors[i]}");
+            }
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine();
-            Console.WriteLine("Please capitalize the first letter. ");
+            Console.WriteLine("(This program will assign a legible text color based on your choice) ");
             Console.WriteLine();
             Console.Write("> ");
-            var screen = Console.ReadLine();
-            //var finalScreen = screen.ToString().ToUpper();
-            
-            //var finalText = text.ToString().ToUpper();
-            // Attempt to parse the colors that the user entered into their respective enum values.
-            // The new values of background and foreground will be set to the user input
-            if (Enum.TryParse(screen, out ConsoleColor background))
+            string bg = Console.ReadLine();
+            try
             {
-                Console.BackgroundColor = background;
+                int pick = (int.Parse(bg) -1);
+                Console.BackgroundColor = colors[pick];
+                if (pick > 9)
+                {
+                Console.ForegroundColor = ConsoleColor.Black;
+                }
+                else if (pick == 7)
+                {
+                Console.ForegroundColor = ConsoleColor.Black;
+                }
+                Console.Clear();
             }
-            
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Clear();
-            Thread.Sleep(300);
+            catch (Exception ex)
+            {
+                Console.WriteLine("I don't understand what you typed! Type a valid number");
+            }
+            Thread.Sleep(250);
             Console.WriteLine("You have such a nice aura today!");
-            Console.WriteLine(" ");
+            Console.WriteLine("");
             
             // MainMenuManager implements the IUserInterfaceManager interface
             IUserInterfaceManager ui = new MainMenuManager();
