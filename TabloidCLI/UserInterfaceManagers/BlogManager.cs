465ab88871df88a93bf5803d00bed988acdd9595
Autumn Fotopoulos <acarte94@gmail.com>
2021-06-17 11:51:57 -0500

    finished blog details

--- TabloidCLI/UserInterfaceManagers/BlogManager.cs
+++ TabloidCLI/UserInterfaceManagers/BlogManager.cs
@@ -22,9 +22,10 @@ namespace TabloidCLI.UserInterfaceManagers
         {
             Console.WriteLine("Blog Menu");
             Console.WriteLine(" 1) List Blog");
-            Console.WriteLine(" 2) Add Blog");
-            Console.WriteLine(" 3) Edit Blog");
-            Console.WriteLine(" 4) Remove Blog");
+            Console.WriteLine(" 2) Veiw Blog Details");
+            Console.WriteLine(" 3) Add Blog");
+            Console.WriteLine(" 4) Edit Blog");
+            Console.WriteLine(" 5) Remove Blog");
             Console.WriteLine(" 0) Go Back");
 
             Console.Write("> ");
@@ -35,12 +36,22 @@ namespace TabloidCLI.UserInterfaceManagers
                     List();
                     return this;
                 case "2":
+                    Blog blog = Choose();
+                    if (blog == null)
+                    {
+                        return this;
+                    }
+                    else
+                    {
+                        return new BlogDetailManager(this, _connectionString, blog.Id);
+                    }
+                case "3":
                     Add();
                     return this;
-                case "3":
+                case "4":
                     Edit();
                     return this;
-                case "4":
+                case "5":
                     Remove();
                     return this;
                 case "0":
