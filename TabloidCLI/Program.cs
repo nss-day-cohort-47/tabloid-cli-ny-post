ae3d1d5fdcf4793837a9b7d43aafa01e9d815757
Jackson <jacksonrgoodman@gmail.com>
2021-06-17 15:48:48 -0500

    restored color decider, patch for post tags

--- TabloidCLI/Program.cs
+++ TabloidCLI/Program.cs
@@ -12,38 +12,38 @@ namespace TabloidCLI
         {
 
 
-            //Console.WriteLine("Please enter desired background color for this screen. ");
-            //Console.WriteLine();
-            //ConsoleColor[] colors = (ConsoleColor[])ConsoleColor.GetValues(typeof(ConsoleColor));
-            //for (int i = 0; i < 16; i++)
-            //{    
-            //    Console.WriteLine($"{i + 1}) {colors[i]}");
-            //}
-            //Console.ForegroundColor = ConsoleColor.White;
-            //Console.WriteLine();
-            //Console.WriteLine("(This program will assign a legible text color based on your choice) ");
-            //Console.WriteLine();
-            //Console.Write("> ");
-            //string bg = Console.ReadLine();
-            //try
-            //{
-            //    int pick = (int.Parse(bg) -1);
-            //    Console.BackgroundColor = colors[pick];
-            //    if (pick > 9)
-            //    {
-            //    Console.ForegroundColor = ConsoleColor.Black;
-            //    }
-            //    else if (pick == 7)
-            //    {
-            //    Console.ForegroundColor = ConsoleColor.Black;
-            //    }
-            //    Console.Clear();
-            //}
-            //catch (Exception ex)
-            //{
-            //    Console.WriteLine("I don't understand what you typed! Type a valid number");
-            //}
-            //Thread.Sleep(250);
+            Console.WriteLine("Please enter desired background color for this screen. ");
+            Console.WriteLine();
+            ConsoleColor[] colors = (ConsoleColor[])ConsoleColor.GetValues(typeof(ConsoleColor));
+            for (int i = 0; i < 16; i++)
+            {
+                Console.WriteLine($"{i + 1}) {colors[i]}");
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+            Console.WriteLine("(This program will assign a legible text color based on your choice) ");
+            Console.WriteLine();
+            Console.Write("> ");
+            string bg = Console.ReadLine();
+            try
+            {
+                int pick = (int.Parse(bg) - 1);
+                Console.BackgroundColor = colors[pick];
+                if (pick > 9)
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                }
+                else if (pick == 7)
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                }
+                Console.Clear();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("I don't understand what you typed! Type a valid number");
+            }
+            Thread.Sleep(250);
             Console.WriteLine("You have such a nice aura today!");
             Console.WriteLine("");
             
